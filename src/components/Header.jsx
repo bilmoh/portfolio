251ff31377105780250253a7748ea3b5fa7ce771
bilmoh <87103425+bilmoh@users.noreply.github.com>
2024-07-14 15:46:19 +0100

    Updated files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link";
 
+import profile from "/assets/img/avatar.jpeg"
+
 export default function Header() {
     const [menuActive, setMenuActive] = useState(false);
 
@@ -17,7 +19,7 @@ export default function Header() {
             <div className="header__content">
                 <div className="header__logo-container" onClick={closeMenu}>
                     <div className="header__logo-img-cont">
-                        <img src="/assets/img/avatar.jpeg" alt="Billal Mohammad Logo Image" className="header__logo-img" />
+                        <img src={profile} alt="Billal Mohammad Logo Image" className="header__logo-img" />
                     </div>
                     <Link className="header__logo-sub" to="/">BILMOH_CODES</Link>
                 </div>
